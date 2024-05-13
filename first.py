9f36c686b320e29d7a30186c2fe1b21448f732ba
unknown <qw5nzwx@gmail.com>
2024-05-13 09:02:14 -0500

    first_poo_update

--- first.py
+++ first.py
@@ -1,4 +1,26 @@
-nombre = "Carlos"
-edad = 22
-m = True
-print("Nombre: {}\nEdad: {}\nMatriculado: {}".format(nombre, edad, m))
+
+
+# clase obrero
+
+class Obrero:
+    def __init__(self, nombre, edad, peso, talla):
+        self.nombre = nombre
+        self.edad = edad
+        self.peso = peso
+        self.talla = talla
+
+    def imc(self):
+        imc = self.peso / (self.talla)**2
+        if imc < 18.5:
+            return ("Bajo peso", imc)
+        elif imc >= 18.5 and imc < 24.9:
+            return ("Normal", imc)
+        elif imc >= 25 and imc <= 29.9:
+            return ("Sobrepeso", imc)
+        elif imc >= 30:
+            return ("Obesidad", imc)
+
+
+init = Obrero("Juan", 30, 70, 1.70)
+print(init.nombre, "cuya edad es", init.edad,
+      "tiene un imc determinado como -> ", init.imc())
